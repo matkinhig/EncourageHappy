967f7f6bf2d67e30a2b7c6a801902495d6dbe9c0
matkinhig <matkinhig@gmail.com>
2019-07-20 01:11:55 -0700

    first commit

---
+++ EncourageHappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EncourageHappy
+//
+//  Created by Nguyễn Lực on 7/20/19.
+//  Copyright © 2019 Nguyễn Lực. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
