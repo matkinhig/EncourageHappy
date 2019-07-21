620eeee9134fb4cf7b52e2c2c1c16e22bc96d1d3
mkig <matkinhig@gmail.com>
2019-07-21 22:16:40 +0700

    Add many Fucntion to application

---
+++ EncourageHappy/CategoriesController.swift
@@ -0,0 +1,25 @@
+//
+//  CategoriesController.swift
+//  EncourageHappy
+//
+//  Created by  Lực Nguyễn on 7/21/19.
+//  Copyright © 2019 Nguyễn Lực. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesController : UIViewController{
+    var type : Int = -1
+    
+    @IBAction func btnAction(_ sender: UIButton) {
+        type = sender.tag
+        performSegue(withIdentifier: "SegueBackToQuote", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SegueBackToQuote" {
+            let destinationViewController = segue.destination as! ViewController
+            destinationViewController.quoteType = 0
+        }
+    }
+}
